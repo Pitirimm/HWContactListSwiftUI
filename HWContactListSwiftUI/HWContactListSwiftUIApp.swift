98b6001fb99e331184bf475faea8df57600d6d94
Yuri Martinovsky <vvrite@me.com>
2021-08-11 11:50:34 +0300

    Initial Commit

---
+++ HWContactListSwiftUI/HWContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWContactListSwiftUIApp.swift
+//  HWContactListSwiftUI
+//
+//  Created by Yuriy Martinovskiy on 11.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct HWContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
